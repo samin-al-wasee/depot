810bd1ac082271323d5f4ab2f3991cdb14ab4a54
Mohammad Samin-Al-Wasee <sameenwasee@gmail.com>
2024-04-01 15:08:06 +0600

    Add cart functionality for products
    - Add cart and line items models, views and controllers
    - Add fixtures
    - Add unit and functional tests for cart and line items

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,4 +1,6 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
@@ -6,44 +8,60 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     @title = "The Great Book #{rand(1000)}"
   end
 
-  test "should get index" do
+  test 'should get index' do
     get products_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_product_url
     assert_response :success
   end
 
-  test "should create product" do
-    assert_difference("Product.count") do
-      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @title } }
+  test 'should create product' do
+    assert_difference('Product.count') do
+      post products_url,
+           params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price,
+                                title: @title } }
     end
 
     assert_redirected_to product_url(Product.last)
   end
 
-  test "should show product" do
+  test 'should show product' do
     get product_url(@product)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_product_url(@product)
     assert_response :success
   end
 
-  test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @title } }
+  test 'should update product' do
+    patch product_url(@product),
+          params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price,
+                               title: @title } }
     assert_redirected_to product_url(@product)
   end
 
-  test "should destroy product" do
-    assert_difference("Product.count", -1) do
+  test 'should destroy product' do
+    assert_difference('Product.count', -1) do
       delete product_url(@product)
     end
 
     assert_redirected_to products_url
+  end
+
+  test "can't delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+    end
+
+    assert_redirected_to products_url
+
+    follow_redirect!
+
+    assert_select 'p', 'Product cannot be deleted as it is in a cart'
   end
 end
